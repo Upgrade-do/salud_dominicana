5d7ab301a906e5cc5b09208a55ed9da858d18e5c
Jose Frometa <jose.frometa@ibanwallet.com>
2021-02-07 20:35:59 -0400

    adds files for freeze and g

---
+++ lib/src/data/model/insurance.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'insurance.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+// ignore: non_constant_identifier_names
+_$_Insurance _$_$_InsuranceFromJson(Map<String, dynamic> json) {
+  return _$_Insurance(
+    (json['insuranceId'] as num)?.toDouble(),
+    json['name'] as String,
+  );
+}
+
+Map<String, dynamic> _$_$_InsuranceToJson(_$_Insurance instance) =>
+    <String, dynamic>{
+      'insuranceId': instance.insuranceId,
+      'name': instance.name,
+    };
