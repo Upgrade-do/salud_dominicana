9267f303e1f1ecb216fd4a75cc2a0acb4a3e423c
Jose Frometa <jose.frometa@ibanwallet.com>
2021-02-08 11:12:28 -0400

    adds linter, and starts testing mocks for http request

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:salud_dominicana/src/list_page.dart';
+import 'package:flutter_riverpod/all.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +10,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Salud Dominicana',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return ProviderScope(
+        child: MaterialApp(
+          title: 'Salud Dominicana',
+          theme: ThemeData(
+            primarySwatch: Colors.red,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: ListPage(),
+    )
     );
   }
 }
--- lib/src/list_page.dart
+++ lib/src/list_page.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:salud_dominicana/src/data/model/insurance.dart';
 import 'package:salud_dominicana/src/data/repositories/insurance_repository.dart';
 import 'package:salud_dominicana/src/data/repositories/mock_insurance_repository.dart';
+import 'package:flutter_riverpod/all.dart';
+
+//fake request
+class FakeHTTPClient {
+  Future<String> get(String url) async {
+    await Future<String>.delayed(const Duration(seconds: 1));
+    return 'Response from $url';
+  }
+
+  Future<List<Insurance>> getInsurance() async {
+    await Future<Insurance>.delayed(const Duration(seconds: 1));
+    return [Insurance(100, 'Name person')];
+  }
+}
+
+
+final fakeHttpClientProvider = Provider((ref) => FakeHTTPClient());
+
+final responseProvider = FutureProvider.autoDispose.family<String, String>( (ref, url) async {
+  final http = ref.read(fakeHttpClientProvider);
+  return http.get(url);
+});
+
+final responseInsuranceProvider = FutureProvider<List<Insurance>>( (ref) async {
+  final http = ref.read(fakeHttpClientProvider);
+  return http.getInsurance();
+});
 
 class ListPage extends StatelessWidget {
   final InsuranceRepository _db = MockInsuranceRepository();
@@ -9,18 +37,28 @@ class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<List<Insurance>>(
-        stream: _db.getInsurances(),
+        stream: _db.getInsurances().first.asStream(),
         builder:
             (BuildContext context, AsyncSnapshot<List<Insurance>> insurances) {
-          if (!insurances.hasData)
+          if (!insurances.hasData) {
             return Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Padding(
                     padding: const EdgeInsets.all(80.0),
-                    child: Text("Loading..."))
+                    child: Consumer(
+                        builder: (context, watch, child) {
+                          final responseAsyncValue = watch(responseProvider('dime th'));
+                          return responseAsyncValue.map(
+                              data:  (_) => Text(_.value),
+                              loading: (_) => CircularProgressIndicator(),
+                              error: (_) => Text(_.error.toString()));
+                        },
+                    ),
+                ),
               ],
             );
+          }
           if (insurances.data.length == 0)
             return Center(
               child: ListView(
